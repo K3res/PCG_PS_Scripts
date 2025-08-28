650248a798bc5fa3268e81b1d3bf3015d42e2d05
Keres <89378576+K3res@users.noreply.github.com>
2025-08-28 12:04:01 +0200

    Update Testing_local.ps1

--- Testing_local.ps1
+++ Testing_local.ps1
@@ -1,2 +1,8 @@
-$resultString = "Verfügbare Laufwerke: $((Get-PSDrive | Where-Object {$_.Free -gt 1} | Select-Object -ExpandProperty Name) -join ', ')"
-$resultString
+# First, get the list of drives as a string
+$driveString = (Get-PSDrive | Where-Object {$_.Free -gt 1} | Select-Object -ExpandProperty Name) -join ', '
+
+# Now, assign this string to the special variable for the tool
+$SRXEnv.ResultMessage = "Verfügbare Laufwerke: $driveString"
+
+# You can still output it for testing in a normal console
+$SRXEnv.ResultMessage
